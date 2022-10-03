50e5fdb73e336ed00f7ed546e693d056c13bd661
Minchul Joh <fclemonschool@gmail.com>
2022-10-03 18:49:28 +0900

    feat(utils): add IoUtils
    Add getUserInput(사용자 입력 콘솔에서 읽기) to IoUtils
    Apply validateInput
    Add dependency(lombok, logging)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,15 @@
 package baseball;
 
+import baseball.utils.IoUtils;
 import baseball.utils.RandomUtils;
+import baseball.utils.ValidationUtils;
 
 public class Application {
     public static void main(String[] args) {
         boolean notEnd = true;
         String randomNumber = RandomUtils.generateRandomNumber();
+
+        String inputValue = IoUtils.getUserInput("숫자를 입력해주세요: ");
+        ValidationUtils.validateInput(inputValue);
     }
 }
