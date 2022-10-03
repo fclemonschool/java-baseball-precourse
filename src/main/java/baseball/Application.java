3ebdb023353346003e287e38d6a8ede7da7ea312
Minchul Joh <fclemonschool@gmail.com>
2022-10-03 19:08:13 +0900

    feat(model): add User
    Add User model
    Add getUserInput and validateInput to User

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,13 @@
 package baseball;
 
-import baseball.utils.IoUtils;
+import baseball.model.User;
 import baseball.utils.RandomUtils;
-import baseball.utils.ValidationUtils;
 
 public class Application {
     public static void main(String[] args) {
         boolean notEnd = true;
         String randomNumber = RandomUtils.generateRandomNumber();
 
-        String inputValue = IoUtils.getUserInput("숫자를 입력해주세요: ");
-        ValidationUtils.validateInput(inputValue);
+        User user = new User();
     }
 }
